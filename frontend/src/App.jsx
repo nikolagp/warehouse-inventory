9994149ab7768872fb7ac3f05b74e6edfd574517
petrovskinikolag@gmail.com <petrovskinikolag@gmail.com>
2023-02-08 10:03:42 +0100

    added navbar template

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,10 +3,12 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import Home from './pages/Home';
 import AddProduct from './pages/AddProduct';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
