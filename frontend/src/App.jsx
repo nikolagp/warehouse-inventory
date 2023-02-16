407b98a9ff4a889ea12b8f1f6c3da6cc6e74ca3e
petrovskinikolag@gmail.com <petrovskinikolag@gmail.com>
2023-02-16 08:58:31 +0100

    refactored App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,5 @@
 import './App.css';
-import { useState, useEffect } from 'react';
-import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Register from './pages/Register';
 import UserDashboard from './components/Dashboard/UserDashboard';
@@ -8,49 +7,12 @@ import Sidebar from './components/Sidebar/Sidebar';
 import AddProduct from './pages/AddProduct';
 import Product from './pages/Product';
 import axios from 'axios';
-import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Login from './pages/Login';
-import { AuthContext } from './helpers/AuthContext';
-import { SET_LOGIN } from './redux/features/auth/authSlice';
 
 axios.defaults.withCredentials = true;
 
 function App() {
-  // const [authState, setAuthState] = useState({
-  //   username: '',
-  //   id: 0,
-  //   status: null,
-  // });
-
-  // useEffect(() => {
-  //   axios
-  //     .get('http://localhost:3001/auth/jwt', {
-  //       headers: {
-  //         accessToken: localStorage.getItem('accessToken'),
-  //       },
-  //     })
-  //     .then((response) => {
-  //       localStorage.setItem('accessToken', response.data.accessToken);
-  //       localStorage.setItem('name', response.data.username);
-  //     });
-  // }, []);
-
-  // const logout = () => {
-  //   localStorage.removeItem('accessToken');
-  //   localStorage.removeItem('name');
-  //   localStorage.removeItem('id');
-  //   SET_LOGIN(false);
-  //   // setAuthState({ ...authState, status: false });
-  // };
-  // if (authState.status === null) {
-  //   return (
-  //     <div>
-  //       <h2>Loading...</h2>
-  //     </div>
-  //   );
-  // }
-
   return (
     <div className="App">
       <BrowserRouter>
@@ -84,7 +46,7 @@ function App() {
               </Sidebar>
             }
           />
-          {/* <Route path="/*" element={<Home to="/" />} /> */}
+          <Route path="/*" element={<Home to="/" />} />
         </Routes>
       </BrowserRouter>
     </div>
