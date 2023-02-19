fc798afbc11e6980c049aff26ab5226eda750d41
petrovskinikolag@gmail.com <petrovskinikolag@gmail.com>
2023-02-19 17:33:43 +0100

    update

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,15 +1,8 @@
-import { configureStore, applyMiddleware } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../redux/features/auth/authSlice';
-import createSagaMiddleware from 'redux-saga';
-import productsSaga from './features/productsSaga';
-
-const saga = createSagaMiddleware();
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
   },
-  middleware: [saga],
 });
-
-saga.run(productsSaga);
